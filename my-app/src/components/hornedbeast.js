dd83ece92e554183265880e6c2fbc0c08db79918
Maram Abu Murad <maramabumurad97@gmail.com>
2021-06-28 20:07:35 +0300

    lab-02

--- my-app/src/components/hornedbeast.js
+++ my-app/src/components/hornedbeast.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
-
 class hornedbeasts extends React.Component{
+ constructor(props){
+ super(props)
+ this.state={
+  votes:0
+ }
+ }
+countingVotes = () =>{
+    this.setState ({
+        votes: this.state.votes +1
+    })
+}
+
     render(){
         return(
-            <div>
+            <div className="beast">
                 
             <h3>
                 {this.props.title}
             </h3>
-            <img src={this.props.image_url}/>
+            <img  src={this.props.image_url}  alt={this.props.title}/>
             <p>
                 {this.props.description}
             </p>
+
+            <p> Number of Votes: {this.state.votes}</p>
+
+            <button onClick={this.countingVotes}>
+                Vote
+            </button>
             
         </div>
         )
@@ -20,4 +37,5 @@ class hornedbeasts extends React.Component{
     }
 }
 
-export default hornedbeasts
+
+export default hornedbeasts;
