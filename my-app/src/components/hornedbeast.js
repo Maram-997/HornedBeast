12262a5859ff634a10c210d3c84438b9af94156f
Maram Abu Murad <maramabumurad97@gmail.com>
2021-06-29 10:26:00 +0300

    fixing css

--- my-app/src/components/hornedbeast.js
+++ my-app/src/components/hornedbeast.js
@@ -2,50 +2,52 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-class hornedbeasts extends React.Component{
- constructor(props){
- super(props)
- this.state={
-  votes:0
- }
- }
-countingVotes = () =>{
-    this.setState ({
-        votes: this.state.votes +1
-    })
-}
+class hornedbeasts extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            votes: 0
+        }
+    }
+    countingVotes = () => {
+        this.setState({
+            votes: this.state.votes + 1
+        })
+    }
 
-    render(){
-        return(
-        //     <div className="beast">
-                
-        //     <h3>
-        //         {this.props.title}
-        //     </h3>
-        //     <img  src={this.props.image_url}  alt={this.props.title}/>
-        //     <p>
-        //         {this.props.description}
-        //     </p>
+    render() {
+        return (
+            //     <div className="beast">
 
-        //     <p> Number of Votes: {this.state.votes}</p>
+            //     <h3>
+            //         {this.props.title}
+            //     </h3>
+            //     <img  src={this.props.image_url}  alt={this.props.title}/>
+            //     <p>
+            //         {this.props.description}
+            //     </p>
 
-        //     <button onClick={this.countingVotes}>
-        //         Vote
-        //     </button>
-            
-        // </div>
+            //     <p> Number of Votes: {this.state.votes}</p>
 
-        <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={this.props.image_url} alt={this.props.title} />
-  <Card.Body>
-    <Card.Title>{this.props.title}</Card.Title>
-    <Card.Text>
-     {this.props.description}
-    </Card.Text>
-    <Card.Text>{this.state.votes} </Card.Text>
-    <Button  onClick={this.countingVotes} >Vote</Button>
-  </Card.Body>
-  </Card>
+            //     <button onClick={this.countingVotes}>
+            //         Vote
+            //     </button>
+
+            // </div>
+
+            <div className="beast">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.image_url} alt={this.props.title} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                        {this.props.description}
+                    </Card.Text>
+                    <Card.Text>Number Of Votes:{this.state.votes} </Card.Text>
+                    <Button onClick={this.countingVotes} >Vote</Button>
+                </Card.Body>
+            </Card>
+            </div>
         )
 
     }
